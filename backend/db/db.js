97df9529e293f87dce1f8c764431ea8514285f72
Daisuke <skd.itkn@gmail.com>
2024-04-24 17:13:08 -0700

    db changed to elephantPG

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,25 +4,14 @@ import dotenv from 'dotenv';
 const { Pool } = pkg;
 dotenv.config();
 
-
-// const pool = new Pool({
-//   user: process.env.user2,
-//   host: process.env.host2,
-//   password: process.env.password2,
-//   database: process.env.database2,
-//   port: process.env.port_db2,
-//   max: 4
-// });
-
 const pool = new Pool({
-  user: 'xndmjiao',
-  host: 'otto.db.elephantsql.com',
-  password: 'Sbx1LLihVeA9kNP4HAhCW12zmwF9RApF',
-  database: 'xndmjiao',
-  port: 5432,
+  user: process.env.user,
+  host: process.env.host,
+  password: process.env.password,
+  database: process.env.database,
+  port: process.env.port_db,
   max: 4
 });
-
 
 const addition1='?ssl=true'
 const addition2='?sslmode=no-verify'
